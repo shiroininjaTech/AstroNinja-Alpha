c0f69e386c25dc893d48421af9a4f77d4d028aee
shiroininjaTech <47760502+shiroininjaTech@users.noreply.github.com>
2020-06-24 20:29:08 -0400

    Changed install script to include chmod commands
    Moved the commands making the desktop icon and astroninjamain.py files executable from instructions for the users to perform in the readme to tasks in the install script.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # A simple Bash shell script that installs packages depended on by AstroNinja
 # Created by: Tom Mullins
 # Created: 10/01/2018
-# Modified: 06/13/2020
+# Modified: 06/24/2020
 
 # First, we need to install the proper python 3 Libraries
 sudo apt-get install -y python3-pip python3-pyqt5 python3-dateutil python3-tk python3-pyqt5.qtwebengine python3-setuptools
@@ -20,8 +20,12 @@ mkdir /home/$USER/.AstroNinja
 cd "${0%/*}"
 cp -r ./* /home/$USER/.AstroNinja
 
-chmod +x /home/$USER/.AstroNinja/AstroNinja.desktop
+# moving the desktop shortcut to the desktop
 mv /home/$USER/.AstroNinja/AstroNinja.desktop /home/$USER/Desktop/
+
+# Making both the desktop file and AstroNinjaMain.py executable
+chmod +x /home/$USER/Desktop/AstroNinja.desktop
+chmod +x /home/$USER/.AstroNinja/AstroNinjaMain.py
 
 # Getting which distro the user is running
 if [ -f /etc/os-release ]; then

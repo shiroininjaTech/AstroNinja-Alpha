2a8d07c80e997aa0b236b8ab5a39605881f356ab
shiroininjaTech <47760502+shiroininjaTech@users.noreply.github.com>
2020-06-30 11:49:17 -0400

    Added a fix for crashing when not reaching YouTube
    Instead of crashing when trying to scrape YouTube, the app will now loop until it gets a connection.

--- youtubeTest.py
+++ youtubeTest.py
@@ -6,7 +6,7 @@
 """
    * Written By : Tom Mullins
    * Created:  04/05/18
-   * Modified: 02/01/19
+   * Modified: 06/30/20
 """
 
 import re
@@ -17,21 +17,26 @@ import requests, bs4
 
 def testFlight():
     # Getting the website and making sure we've made contact :P
-    spacexChannel = requests.get('https://www.youtube.com/user/spacexchannel/videos?flow=grid&view=0&sort=dd')
-    spacexChannel.raise_for_status()
 
-    # Now that we have the website loaded, let's parse it with Beautiful Soup (why not Ugly soup??)
-    xSoup = bs4.BeautifulSoup(spacexChannel.text, "lxml")
-
-
-    # Getting the Date and Mission name
-    findVids = xSoup.find_all("a", class_ = "yt-uix-tile-link")
-    global fullURL, newestName
     links = []
-    names = []
-    for x in findVids:
-        links.append(x.get("href"))
-        names.append(x.get("title"))
+
+    # If connection to YouTube fails, keep trying.
+    while len(links) == 0:
+        spacexChannel = requests.get('https://www.youtube.com/user/spacexchannel/videos?flow=grid&view=0&sort=dd')
+        spacexChannel.raise_for_status()
+
+        # Now that we have the website loaded, let's parse it with Beautiful Soup (why not Ugly soup??)
+        xSoup = bs4.BeautifulSoup(spacexChannel.text, "lxml")
+
+
+        # Getting the Date and Mission name
+        findVids = xSoup.find_all("a", class_ = "yt-uix-tile-link")
+        global fullURL, newestName
+
+        names = []
+        for x in findVids:
+            links.append(x.get("href"))
+            names.append(x.get("title"))
     youtubeURL = 'https://www.youtube.com/embed/'
     newestLink = links[0]
     newestLink2 = newestLink[9:]

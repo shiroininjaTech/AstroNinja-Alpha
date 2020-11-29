af5ceac0af4af719dc13ab4a0775b7389a734475
shiroininjaTech <47760502+shiroininjaTech@users.noreply.github.com>
2020-11-29 11:18:50 -0500

    removed Cyber Monday telescope reviews from news feed

--- AstroNinjaMain.py
+++ AstroNinjaMain.py
@@ -8,7 +8,7 @@
    * Written By: Tom Mullins
    * Version: 0.85
    * Date Created:  10/13/17
-   * Date Modified: 11/27/20
+   * Date Modified: 11/29/20
 """
 """
    * Changelog:
@@ -863,8 +863,8 @@ class App(QMainWindow):
         def newsListBuilder(a, b, c, d, e):
 
             # Removing unwanted articles from space.com
-            naughtyArticles = ['Pictures from Space!', 'The top space stories of the week!', 'Black Friday', 'Best telescopes']                        # The list of articles to look for
-            if naughtyArticles[0] in xNewsV85.listedTitle[a] or naughtyArticles[1] in xNewsV85.listedTitle[a]  or naughtyArticles[2] in xNewsV85.listedTitle[a] or naughtyArticles[3] in xNewsV85.listedTitle[a]:      # if the title matches one of naughtyArticles
+            naughtyArticles = ['Pictures from Space!', 'The top space stories of the week!', 'Black Friday', 'Best telescopes', 'Cyber Monday']                        # The list of articles to look for
+            if naughtyArticles[0] in xNewsV85.listedTitle[a] or naughtyArticles[1] in xNewsV85.listedTitle[a]  or naughtyArticles[2] in xNewsV85.listedTitle[a] or naughtyArticles[3] in xNewsV85.listedTitle[a] or naughtyArticles[4] in xNewsV85.listedTitle[a]:      # if the title matches one of naughtyArticles
                 # Ends and moves on to the next article if found.
                 return
 
